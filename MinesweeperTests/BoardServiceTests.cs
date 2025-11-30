5205909db94c08e367503aa5bc3cd815f9d0ae8a
Angela Alanis <angelaalanis49@yahoo.com>
2025-11-29 21:54:16 -0600

    Finished Milestone 1: models, BLL, console output, and xUnit tests added.

--- MinesweeperTests/BoardServiceTests.cs
+++ MinesweeperTests/BoardServiceTests.cs
@@ -57,6 +57,7 @@ namespace MinesweeperTests
                 {0,0},{0,1},{0,2},
                 {1,0},       {1,2},
                 {2,0},{2,1},{2,2}
+
             };
 
             for (int i = 0; i < neighbors.GetLength(0); i++)
